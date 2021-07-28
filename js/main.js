9950cbb6aa77ad9ea41f19ebe7ee8521bef330e8
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-07-28 11:57:32 -0700

    Add data model, local storage creation, button to accept or reject fox, and adding fox image url to local storage and data model

--- js/main.js
+++ js/main.js
@@ -3,12 +3,36 @@ var summonBtn = document.querySelector('.summon-btn');
 
 var summonImg = document.querySelector('.image');
 
+var acceptBtnDiv = document.querySelector('.accept-btn-div');
+
+var rejectBtn = document.querySelector('.reject-btn');
+
 var acceptBtn = document.querySelector('.accept-btn');
 
 
-function getLoadFox(){
+// var croppedWrong = ['https://randomfox.ca/images/10.jpg', 'https://randomfox.ca/images/7.jpg', 'https://randomfox.ca/images/22.jpg', 'https://randomfox.ca/images/1.jpg', 'https://randomfox.ca/images/17.jpg', 'https://randomfox.ca/images/1.jpg', 'https://randomfox.ca/images/30.jpg', 'https://randomfox.ca/images/102.jpg', 'https://randomfox.ca/images/38.jpg', 'https://randomfox.ca/images/4.jpg', 'https://randomfox.ca/images/14.jpg', 'https://randomfox.ca/images/23.jpg', 'https://randomfox.ca/images/35.jpg', 'https://randomfox.ca/images/6.jpg']
 
-  var image = ''
+
+function getMeta(url) {
+  var img = new Image();
+  img.addEventListener("load", function () {
+    if (this.naturalWidth + 100 < this.naturalHeight) {
+      console.log('image is too long')
+      var warningText = document.querySelector('.warning');
+      warningText.textContent = 'This fox seems to be off their center. Bad vibes! Please proceed with caution';
+    } else {
+      var warningText = document.querySelector('.warning');
+      warningText.textContent = '';
+    }
+  });
+  img.src = url;
+}
+
+var image = ''
+
+function getLoadFox() {
+
+
 
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://randomfox.ca/floof/');  // http://shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]
@@ -20,6 +44,13 @@ function getLoadFox(){
     console.log(xhr.response);
 
     summonImg.setAttribute("src", xhr.response.image)
+    getMeta(xhr.response.image);
+    image = xhr.response.image;
+
+    summonBtn.classList.add('hidden');
+
+    acceptBtnDiv.classList.remove('hidden');
+
 
 
 
@@ -30,4 +61,26 @@ function getLoadFox(){
 
 }
 
-summonBtn.addEventListener('click', getLoadFox)
+function rejectFox() {
+  summonBtn.classList.remove('hidden');
+
+  acceptBtnDiv.classList.add('hidden');
+
+  summonImg.setAttribute("src", 'images/fox.png');
+}
+
+function acceptFox() {
+  summonBtn.classList.remove('hidden');
+
+  acceptBtnDiv.classList.add('hidden');
+
+  summonImg.setAttribute("src", 'images/fox.png');
+  data.collection.push({ foxImage: image, quote: '' });
+
+}
+
+summonBtn.addEventListener('click', getLoadFox);
+
+rejectBtn.addEventListener('click', rejectFox);
+
+acceptBtn.addEventListener('click', acceptFox);
