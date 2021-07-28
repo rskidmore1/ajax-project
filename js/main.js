e5652522b23334f329490f5fb11a4dc475d31261
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-07-27 17:30:56 -0700

    Created API call and image load

--- js/main.js
+++ js/main.js
@@ -1,10 +1,39 @@
 
 var summonBtn = document.querySelector('.summon-btn');
 
-function summon(event){
+var summonImg = document.querySelector('.image');
+
+var acceptBtn = document.querySelector('.accept-btn');
+
+
+function getLoadFox(){
+
+  var image = ''
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://randomfox.ca/floof/');  // http://shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]
+
+  var response = [];
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+
+    console.log(xhr.status);
+    console.log(xhr.response);
+
+    response.push(xhr.response);
+
+    image = response[0].image;
+    // console.log(image)
+
+    summonImg.setAttribute("src", image)
+
+
+
+  });
+
+  xhr.send();
+
 
 }
 
-summonBtn.addEventListener('click', function(event){
-console.log('hello world')
-})
+summonBtn.addEventListener('click', getLoadFox)
