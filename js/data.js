cf6905377d5782e90018adb150a544d49ce22ce3
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-07-28 17:36:52 -0700

    Pull data from data local-storage

--- js/data.js
+++ js/data.js
@@ -1,1 +1,18 @@
 /* exported data */
+
+var data = {
+  collection: [
+    { foxImage: '', quote: '', bad: false }
+  ]
+};
+
+var previousFormEntries = localStorage.getItem('collection');
+
+if (previousFormEntries !== null) {
+  data = JSON.parse(previousFormEntries);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var entriesStringified = JSON.stringify(data);
+  localStorage.setItem('collection', entriesStringified);
+});
--- js/main.js
+++ js/main.js
@@ -3,31 +3,72 @@ var summonBtn = document.querySelector('.summon-btn');
 
 var summonImg = document.querySelector('.image');
 
+var acceptBtnDiv = document.querySelector('.accept-btn-div');
+
+var rejectBtn = document.querySelector('.reject-btn');
+
 var acceptBtn = document.querySelector('.accept-btn');
 
+var image = '';
 
-function getLoadFox(){
-
-  var image = ''
+function getLoadFox() {
 
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://randomfox.ca/floof/');  // http://shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]
+  xhr.open('GET', 'https://randomfox.ca/floof/');
 
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
 
-    console.log(xhr.status);
-    console.log(xhr.response);
+    // console.log(xhr.status);
+    // console.log(xhr.response);
 
-    summonImg.setAttribute("src", xhr.response.image)
+    summonImg.setAttribute('src', xhr.response.image);
+    image = xhr.response.image;
 
+    summonBtn.classList.add('hidden');
 
+    acceptBtnDiv.classList.remove('hidden');
 
   });
 
   xhr.send();
 
+}
+
+function rejectFox() {
+  summonBtn.classList.remove('hidden');
+
+  acceptBtnDiv.classList.add('hidden');
+
+  summonImg.setAttribute('src', 'images/pleasesummon.png');
 
 }
 
-summonBtn.addEventListener('click', getLoadFox)
+function acceptFox() {
+  summonBtn.classList.remove('hidden');
+
+  acceptBtnDiv.classList.add('hidden');
+
+  summonImg.setAttribute('src', 'images/pleasesummon.png');
+
+  data.collection.push({ foxImage: image, quote: '' });
+
+}
+
+summonBtn.addEventListener('click', getLoadFox);
+
+rejectBtn.addEventListener('click', rejectFox);
+
+acceptBtn.addEventListener('click', acceptFox);
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  var collectionView = document.querySelectorAll('.collection-img');
+  for (var i = 0; i < data.collection.length; i++) {
+    if (i > 7) {
+      break;
+    } else {
+      collectionView[i].setAttribute('src', data.collection[i].foxImage);
+    }
+
+  }
+});
